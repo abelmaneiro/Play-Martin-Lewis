dfe015179abe3d2afb73403e96fe5c2543e2e22c
abelmaneiro <32059405+abelmaneiro@users.noreply.github.com>
2021-06-20 15:12:22 +0100

    6 Form Data with GET in Play using Scala

--- app/controllers/TaskList1.scala
+++ app/controllers/TaskList1.scala
@@ -7,6 +7,13 @@ import javax.inject.{Inject, Singleton}
 @Singleton  // Juice injection which Play used by default for dependency injection
 class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(cc) { // need to inject
   // the Controller Components and extend the Controller class
+  def login1: Action[AnyContent] = Action {
+    Ok(views.html.login1())
+  }
+  def validateLogin1Get(username: String, password: String): Action[AnyContent] = Action {
+    Ok(s"$username login with $password")
+  }
+
   def taskList: Action[AnyContent] = Action {
     Ok(views.html.taskList1(Seq("Task1", "Task2", "Task3")))   // Because it's a html view it's in the html folder
   }
