281fe7624a0cf55d0be72b20e89417f9e6b62d70
abelmaneiro <32059405+abelmaneiro@users.noreply.github.com>
2021-06-20 12:21:15 +0100

    4 Making Our First View with Play using Scala

--- app/controllers/TaskList1.scala
+++ app/controllers/TaskList1.scala
@@ -6,10 +6,8 @@ import javax.inject.{Inject, Singleton}
 
 @Singleton  // Juice injection which Play used by default for dependency injection
 class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(cc) { // need to inject
-  //the Controller Components and extend the Controller class
-
+  // the Controller Components and extend the Controller class
   def taskList: Action[AnyContent] = Action {
-    Ok("This works")
+    Ok(views.html.taskList1(Seq("Task1", "Task2", "Task3")))   // Because it's a html view it's in the html folder
   }
-
 }
