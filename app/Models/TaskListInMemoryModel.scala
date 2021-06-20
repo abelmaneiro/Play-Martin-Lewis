9a1b43bc057b23df1eb279d2daaabb009b5785c5
abelmaneiro <32059405+abelmaneiro@users.noreply.github.com>
2021-06-20 19:58:30 +0100

    10 Making a Model with Play using Scala (Part 2)

--- app/Models/TaskListInMemoryModel.scala
+++ app/Models/TaskListInMemoryModel.scala
@@ -6,6 +6,9 @@ object TaskListInMemoryModel {
   private val users = mutable.Map[String, String](
     "Mark" -> "pass"
   )
+  private val tasks = mutable.Map[String, List[String]](
+    "Mark"  -> List("Make videos", "eat", "sleep", "code")
+  )
 
   def validateUser(username: String, password: String): Boolean = {
     // users.get(username).map(_ == password).getOrElse(false)
@@ -13,9 +16,20 @@ object TaskListInMemoryModel {
     users.get(username).contains(password)
   }
 
-  def createUser(username: String, password: String): Boolean = ???
+  def createUser(username: String, password: String): Boolean = {
+    if (users.contains(username)) false
+    else {
+      // users += (username -> password)
+      users(username) = password
+      tasks(username) = Nil
+      true
+    }
+  }
 
-  def getTasks(username: String): Seq[String] = ???
+  def getTasks(username: String): Seq[String] = {
+    //tasks.get(username).getOrElse(List())
+    tasks.getOrElse(username, Nil)
+  }
 
   def addTask(username: String, task: String): Unit = ???
 
--- app/controllers/TaskList1.scala
+++ app/controllers/TaskList1.scala
@@ -6,17 +6,19 @@ import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponent
 import javax.inject.{Inject, Singleton}
 
 @Singleton  // Juice injection which Play used by default for dependency injection
-class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(cc) { // need to inject
-  // the Controller Components and extend the Controller class
+class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(cc) { // need to inject the
+                                                            // Controller Components and extend the Controller class
   def login1: Action[AnyContent] = Action {
     Ok(views.html.login1())
   }
+
   def validateLogin1Get(username: String, password: String): Action[AnyContent] = Action {
     Ok(s"$username login with $password")
   }
+
   def validateLogin1Post: Action[AnyContent] = Action { request =>
     val body = request.body.asFormUrlEncoded  //  is type of Option[Map[String, Seq[String]]]
-    val credentials = body.map {args =>   // is type of Option[Result]
+    val credentials = body.map { args =>   // is type of Option[Result]
       val username = args("username").head
       val password = args("password").head
       if (TaskListInMemoryModel.validateUser(username, password))
@@ -28,7 +30,22 @@ class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(c
     result
   }
 
-  def taskList: Action[AnyContent] = Action {
-    Ok(views.html.taskList1(Seq("Task1", "Task2", "Task3")))   // Because it's a html view it's in the html folder
+  def createUser1: Action[AnyContent] = Action { request =>
+    val body = request.body.asFormUrlEncoded  //  is type of Option[Map[String, Seq[String]]]
+    body.map { args =>   // is type of Option[Result]
+      val username = args("username").head
+      val password = args("password").head
+      if (TaskListInMemoryModel.createUser(username, password))
+        Redirect(routes.TaskList1.taskList)  // redirect to taskList page using reverse routing
+      else
+      Redirect(routes.TaskList1.login1)
+    }.getOrElse(Redirect(routes.TaskList1.login1))
   }
+
+  def taskList: Action[AnyContent] = Action {
+    val username = "Mark"
+    val tasks = TaskListInMemoryModel.getTasks(username)
+    Ok(views.html.taskList1(tasks))   // Because it's a html view it's in the html folder
+  }
+
 }
