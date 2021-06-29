824d127b1d87eadc5886bbe4afcda4ddd19172d7
abelmaneiro <32059405+abelmaneiro@users.noreply.github.com>
2021-06-29 19:19:07 +0100

    27 Ajax in Play using Scala (Part 2) Ajax Fletch call

--- public/javascripts/basicStuff.js
+++ public/javascripts/basicStuff.js
@@ -1,7 +1,19 @@
 /* Basic stuff and as it is Public have access to it via the GET /assets/*file routes file
 
  */
-    console.log("Running JS!")
+    console.log("Running JS!");
     $("#randomText").click(function () {
-        $("#random").load("/random")   // calls GET http://localhost:9000/random
-    })
+        $("#random").load("/random");   // calls GET http://localhost:9000/random
+    });
+
+    const stringText = document.getElementById("randomStringText");
+    stringText.onclick = () => {
+        const lengthInput = document.getElementById("lengthValue")
+        console.log(lengthInput)
+        fetch("/randomString/" + lengthInput.value).then((response) => {
+            return response.text()
+        }).then(responseText => {
+            const randomString = document.getElementById("randomString");
+            randomString.innerHTML = responseText;
+        })
+    };
