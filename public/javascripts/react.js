35e9aa96fa75322671134a52e9bae9a54eaa6427
abelmaneiro <32059405+abelmaneiro@users.noreply.github.com>
2021-07-05 23:46:40 +0100

    51, 52, 53 Introduction to React Part 1, 2, 3 (Play using Scala) With new route /react

---
+++ public/javascripts/react.js
@@ -0,0 +1,51 @@
+console.log("Running version4.js")
+
+const ce = React.createElement
+
+function StatelessHello(props){
+    return ce('div', null, `Hello ${props.toWhat}`);
+}
+
+class Hello extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { clickCount : 0};
+    }
+    render() {
+        return ce('div',
+            {onClick: (e) => this.clickHandler(e)},
+            `Hello ${this.props.toWhat} - click count ${this.state.clickCount}`
+        );
+    }
+    clickHandler() {
+        this.setState({clickCount: this.state.clickCount + 1});
+    }
+}
+
+class SimpleForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {textInput: ""}
+    }
+
+    render() {
+        return ce('input', {
+            type:"text",
+            value: this.state.textInput,
+            onChange: (e) => this.changeHandler(e)}
+        );
+    }
+
+    changeHandler(e) {
+        this.setState({textInput: event.target.value});
+    }
+}
+
+ReactDOM.render(
+    ce('div', null,
+        ce(Hello, {toWhat: 'World'}, null),
+        ce(StatelessHello, {toWhat: 'React'}, null),
+        ce(SimpleForm, null, null)
+    ),
+    document.getElementById('react-root')
+);
